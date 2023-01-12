44fd62e7a5171eac97a034f8491bd679521291c7
Esnola <esnola@gmail.com>
2023-01-12 17:43:32 +0100

    modified:   index2.php

--- index2.php
+++ index2.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebas</title>
+    <link rel="stylesheet" href="./css/bulma.min.css">
+</head>
+<body>
+
+</body>
+</html>
+
 <?php
     require_once "./php/main.php";
+
 $nombre ="Jaaauan Joswwe";
 $apellido="Apellido";
 $usuario ="Esnola";
@@ -17,3 +32,20 @@ $marcadores = [
 ];
 
 $guardar_usuario->execute($marcadores);
+
+if ($guardar_usuario->rowCount() == 1) {
+    echo '
+            <div class="notification is-info is-light">
+                <strong>¡USUARIO REGISTRADO!</strong><br>
+                El usuario se registro con exito
+            </div>
+        ';
+} else {
+    echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                No se pudo registrar el usuario, por favor intente nuevamente
+            </div>
+        ';
+}
+$guardar_usuario = null;
