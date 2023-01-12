5a9a68cec2825fca724385e7ed8f8e55e581f451
Esnola <esnola@gmail.com>
2023-01-12 17:33:58 +0100

    modified:   index2.php

--- index2.php
+++ index2.php
@@ -1,14 +1,19 @@
 <?php
     require_once "./php/main.php";
-    $user ="Juan Jose";
-    $userCheck =  conexion();
-    $userCheck= $userCheck->query("SELECT * FROM usuario WHERE usuario_nombre='$user'");
-    $cantidad = $userCheck->rowCount();
-    $results = $userCheck->fetchAll(PDO::FETCH_OBJ);
-            if($cantidad >0){
-                 foreach ($results as $result) {
-                    echo "<br> Nombre " . $result-> usuario_nombre;
-                }
-            }else{
-                echo "No se ha enccontrado un usuario con ese nombre";
-            }
+$nombre ="Jaaauan Joswwe";
+$apellido="Apellido";
+$usuario ="Esnola";
+$clave ="12345";
+$email="email.com";
+$guardar_usuario = conexion();
+$guardar_usuario = $guardar_usuario->prepare("INSERT INTO usuario(usuario_nombre,usuario_apellido,usuario_usuario,usuario_clave,usuario_email) VALUES(:nombre,:apellido,:usuario,:clave,:email)");
+
+$marcadores = [
+    ":nombre" => $nombre,
+    ":apellido" => $apellido,
+    ":usuario" => $usuario,
+    ":clave" => $clave,
+    ":email" => $email
+];
+
+$guardar_usuario->execute($marcadores);
